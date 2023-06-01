0e950e413aa5b9ca10c05d99b6909a8c9e487db7
AdithyaRaj <113771824+Adithyaa07@users.noreply.github.com>
2023-06-01 20:54:18 +0530

    second commit

---
+++ src/firebase-auth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth" ; 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAQSyQO7nv-TFX_joMron7rhFkTEgL7n68",
+    authDomain: "authentication-aa7d6.firebaseapp.com",
+    projectId: "authentication-aa7d6",
+    storageBucket: "authentication-aa7d6.appspot.com",
+    messagingSenderId: "637196192284",
+    appId: "1:637196192284:web:352393c074632710916dd9"
+  };
+  
+ 
+  const app = initializeApp(firebaseConfig);
+
+export  const auth = getAuth(app);
